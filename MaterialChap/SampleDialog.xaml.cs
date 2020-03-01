475c75d20121433f18ac6d5a479cab0b8b3c914f
nathan1658 <pooikei165@gmail.com>
2020-03-01 09:57:49 +0800

    Added value percentage converter and enhanced http webview

--- MaterialChap/SampleDialog.xaml.cs
+++ MaterialChap/SampleDialog.xaml.cs
@@ -11,7 +11,7 @@ namespace MaterialChap
     {
         public SampleDialog()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
     }
 }
---
+++ MaterialChap/ValuePercentageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MaterialChap
+{
+    public class ValuePercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value!=null && parameter!=null)
+            {
+                double val1 = (double)value;
+                double val2 = double.Parse((string)parameter);
+                return val1 * val2;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
